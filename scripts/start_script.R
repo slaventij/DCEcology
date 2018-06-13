b9288edf79af1aedcc9f520241984f095041447c
slaventij <slaventij34t@bigmir.net>
2018-06-13 12:11:57 +0200

    add codding with dates

--- scripts/start_script.R
+++ scripts/start_script.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(lubridate)
 download.file("https://ndownloader.figshare.com/files/2292169", "data/portal_data_joined.csv")
 surveys <- read.csv("data/portal_data_joined.csv")
 str(surveys)
@@ -10,6 +10,18 @@ plot(surveys$taxa)
 sex <- surveys$sex
 levels(surveys$sex)[1] <- "undetermined"
 levels(surveys$sex)
-levels(surveys$sex)[2] <- "female"
-levels(surveys$sex)[3] <- "male"
+levels(surveys$sex)[2:3] <- c("female","male")
+surveys$sex <- factor(surveys$sex, levels=c( "female", "male","undetermined"))
 plot(surveys$sex)
+summary(surveys$sex)
+
+### working with dates
+my_date <- ymd("2015-01-01")
+#or
+my_date <- ymd(paste("2015","1","1", sep = "-"))
+surveys$date <- ymd(paste(surveys$year, surveys$month, surveys$day, sep = "-"))
+is_missing_date <- is.na(surveys$date)
+date_columns <- c("year", "month", "day")
+missing_dates <- surveys[is_missing_date, date_columns]
+head(missing_dates)
+
